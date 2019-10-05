a0e16e606f1b690037eccbb90008b3ba84a460ea
Roy Ivy III <rivy.dev@gmail.com>
2019-10-05 18:41:48 -0500

    wip ~ add rivy/replace-in-files-cli for coverage comment hack

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,44 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'none',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: 'markdown-attr-parser',
-    libraryTarget: 'commonjs2',
-    globalObject: 'typeof self !== "undefined" ? self : this', // needed for UMD wrapper; ref: <https://github.com/markdalgleish/static-site-generator-webpack-plugin/issues/130#issuecomment-445435821>
+    libraryTarget: 'umd',
+    auxiliaryComment: 'istanbul ignore next', // HACK to remove coverage from ** wrapper code
+    globalObject: 'typeof self !== "undefined" ? self : this', // needed for UMD wrapper; ref: <https://github.com/markdalgleish/static-site-generator-**-plugin/issues/130#issuecomment-445435821>
   },
   module: {
     rules: [
+      // {
+      //   test: /[.]js$/i,
+      //   use: {
+      //     loader: 'istanbul-instrumenter-loader',
+      //     // options: {...options}
+      //   },
+      //   // enforce: 'post',
+      // },
       {
         test: /[.]pegjs$/i,
-        use: 'pegjs-loader?optimize=size',
+        use: [
+          // {
+          //   loader: 'istanbul-instrumenter-loader',
+          //   // options: {...options}
+          // },
+          {
+            loader: 'pegjs-loader?optimize=size',
+          },
+        ],
       },
     ],
   },
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all',
+  //   },
   devServer: {},
-  devtool: 'source-map',
+  devtool: '',
 };
