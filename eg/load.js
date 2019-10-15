8c698405b3c01bbb555ba748c1fb81143b35bcb1
Roy Ivy III <rivy.dev@gmail.com>
2019-10-14 20:04:37 -0500

    change ~ improve example(s)

--- eg/load.js
+++ eg/load.js
@@ -1,3 +1,8 @@
 const parser = require('..');
 
-console.log(parser('{ #id }'));
+var s = '';
+
+s = '{ }'; console.log(s, ' => ', parser(s));
+s = '{ .class }'; console.log(s, ' => ', parser(s));
+s = '{ #id }'; console.log(s, ' => ', parser(s));
+s = '{ key=value #id .class.x.y }'; console.log(s, ' => ', parser(s));
