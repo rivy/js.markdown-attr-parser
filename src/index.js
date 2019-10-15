79207c526f7c41c5a1e70436712e808f1cedc0d8
Roy Ivy III <rivy.dev@gmail.com>
2019-10-14 19:59:59 -0500

    wip/change ~ clean class, id, and key names

--- src/index.js
+++ src/index.js
@@ -21,12 +21,42 @@ function normalizeParserOutput(parsed, config) {
   const retval = {};
   retval.prop = {};
   retval.eaten = match;
+
+  // spell-checker:ignore nonChar ufdef ufeff uffd ufff uffff
+  // BOM "unicode byte-order-mark" = [\ufeff]
+  // UC0 "unicode '[C0] C0 controls'" = [\0-\u001f]
+  // del = [\u007f]
+  // nonchar = [\uffd0\ufdef\ufff3\uffff\u1FFFE\u1FFFF\u2FFFE\u2FFFF\u3FFFE\u3FFFF\u4FFFE\u4FFFF\u5FFFE\u5FFFF\u6FFFE\u6FFFF\u7FFFE\u7FFFF\u8FFFE\u8FFFF\u9FFFE\u9FFFF\uAFFFE\uAFFFF\uBFFFE\uBFFFF\uCFFFE\uCFFFF\uDFFFE\uDFFFF\uEFFFE\uEFFFF\uFFFFE\uFFFFF\u10FFFE\u10FFFF]
+  // char = !(UC0 / del / nonchar) c:. { return c; }
+
+  const UC0 = /[\0-\u001f]/u; /* eslint-disable-line no-control-regex */
+  const UZs = /[\u0020\u00A0\u1680\u2000-\u200A\u202F\u205F\u3000]/; // "unicode '[Zs] Separator,Space'"
+  const Usl = /[\u2028]/u; // "unicode line separator"
+  const Usp = /[\u2029]/u; // "unicode paragraph separator"
+  const del = /[\u007f]/u;
+  const nonchar = /[\uffd0\ufdef\ufff3\uffff\u1FFFE\u1FFFF\u2FFFE\u2FFFF\u3FFFE\u3FFFF\u4FFFE\u4FFFF\u5FFFE\u5FFFF\u6FFFE\u6FFFF\u7FFFE\u7FFFF\u8FFFE\u8FFFF\u9FFFE\u9FFFF\uAFFFE\uAFFFF\uBFFFE\uBFFFF\uCFFFE\uCFFFF\uDFFFE\uDFFFF\uEFFFE\uEFFFF\uFFFFE\uFFFFF\u10FFFE\u10FFFF]/u;
+  const ws = new RegExp(['\\s', UZs.source].join('|'), 'gmu');
+  const eol = new RegExp(['[\n\r]', Usl.source, Usp.source].join('|'), 'gmu');
+  const invisible = new RegExp([UC0.source, del.source, nonchar.source].join('|'), 'gmu');
+
   // * set id from first id tag
   // ... empty IDs are not allowed
   const idElement = attributes && attributes.find(elem => elem.id);
   if (idElement) {
+    // ... clean ID name
+    // idElement.id = idElement.id.replace(ws, '-').replace(eol, '~').replace(invisible, '?');
     retval.prop.id = idElement.id;
   }
+
+  // ToDO: HTMLStringEncode() elem.value
+  // ref: <https://mathiasbynens.be/notes/html5-id-class>
+  // ref: <https://stackoverflow.com/questions/9882257/how-to-reference-a-long-class-name-with-spaces-in-css#comment19004253_9882293>
+  // class names (ie, class attribute values may contain ANY character except NUL; ... and whitespace because of multiple classnames being separated by whitespace)
+  // HTML5 ids are similar; may not be empty, cannot contain spaces, and must be document unique
+  // ToDO: replace any whitespace sequence in class name with '-'
+  //   ... for sanity, replace any invisible characters with '?', as well
+  // ToDO: replace any illegal ID characters with '?'; for HTML5, IDs may not contain any kind of space character and may not be empty, per <http://xahlee.info/js/html_allowed_chars_in_attribute.html>
+  //   ... for sanity, replace any invisible characters with '?', as well
 
   attributes.forEach(elem => {
     // * convert class key to class type value
@@ -39,6 +69,9 @@ function normalizeParserOutput(parsed, config) {
     if (elem.class) {
       // * concat any new unique classes
       retval.prop.class = retval.prop.class || [];
+      // ... clean class name(s)
+      // elem.class = elem.class.map(e => e.replace(ws, '-').replace(eol, '~').replace(invisible, '?').replace(/[.]/g, '-'));
+      elem.class = elem.class.map(e => e.replace(ws, '-').replace(eol, '~').replace(invisible, '?'));
       retval.prop.class = retval.prop.class.concat(elem.class.filter(item => item && retval.prop.class.indexOf(item) < 0));
       if (retval.prop.class.length === 0) {
         delete retval.prop.class;
@@ -52,38 +85,12 @@ function normalizeParserOutput(parsed, config) {
       }
     } else {
       // ToDO: discard if case-insensitive elem.key matches any already set property per <https://html.spec.whatwg.org/multipage/syntax.html#attributes-2>
+      // ... clean key name
+      elem.key = elem.key.replace(ws, '-').replace(eol, '~').replace(invisible, '?');
       retval.prop[elem.key] = retval.prop[elem.key] || (typeof elem.value === 'undefined' ? config.defaultValue(elem.key) : elem.value);
     }
   });
   // retval._trace = a;
-
-  // ToDO: HTMLStringEncode() elem.value
-  // ref: <https://mathiasbynens.be/notes/html5-id-class>
-  // ref: <https://stackoverflow.com/questions/9882257/how-to-reference-a-long-class-name-with-spaces-in-css#comment19004253_9882293>
-  // class names (ie, class attribute values may contain ANY character except NUL; ... and whitespace because of multiple classnames being separated by whitespace)
-  // HTML5 ids are similar; may not be empty, cannot contain spaces, and must be document unique
-  // ToDO: replace any whitespace sequence in class name with '-'
-  //   ... for sanity, replace any invisible characters with '?', as well
-  // ToDO: replace any illegal ID characters with '?'; for HTML5, IDs may not contain any kind of space character and may not be empty, per <http://xahlee.info/js/html_allowed_chars_in_attribute.html>
-  //   ... for sanity, replace any invisible characters with '?', as well
-
-  // spell-checker:ignore nonChar ufdef ufeff uffd ufff uffff
-  // BOM "unicode byte-order-mark" = [\ufeff]
-  // UC0 "unicode '[C0] C0 controls'" = [\0-\u001f]
-  // del = [\u007f]
-  // nonchar = [\uffd0\ufdef\ufff3\uffff\u1FFFE\u1FFFF\u2FFFE\u2FFFF\u3FFFE\u3FFFF\u4FFFE\u4FFFF\u5FFFE\u5FFFF\u6FFFE\u6FFFF\u7FFFE\u7FFFF\u8FFFE\u8FFFF\u9FFFE\u9FFFF\uAFFFE\uAFFFF\uBFFFE\uBFFFF\uCFFFE\uCFFFF\uDFFFE\uDFFFF\uEFFFE\uEFFFF\uFFFFE\uFFFFF\u10FFFE\u10FFFF]
-  // char = !(UC0 / del / nonchar) c:. { return c; }
-
-  // const UC0 = /[\0-\001f]/;
-  // const UZs = /[\u0020\u00A0\u1680\u2000-\u200A\u202F\u205F\u3000]/; // "unicode '[Zs] Separator,Space'"
-  // const Usl = /[\u2028]/; // "unicode line separator"
-  // const Usp = /[\u2029]/; // "unicode paragraph separator"
-  // const del = /[\u007f]/;
-  // const nonchar = /[\uffd0\ufdef\ufff3\uffff\u1FFFE\u1FFFF\u2FFFE\u2FFFF\u3FFFE\u3FFFF\u4FFFE\u4FFFF\u5FFFE\u5FFFF\u6FFFE\u6FFFF\u7FFFE\u7FFFF\u8FFFE\u8FFFF\u9FFFE\u9FFFF\uAFFFE\uAFFFF\uBFFFE\uBFFFF\uCFFFE\uCFFFF\uDFFFE\uDFFFF\uEFFFE\uEFFFF\uFFFFE\uFFFFF\u10FFFE\u10FFFF]/;
-  // const ws =
-  // if (retval.prop.class) {
-  //   retval.prop.class.forEach(function(c) {})
-  // }
 
   return retval;
 }
