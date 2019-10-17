0c4fbc293e6f9ae6dd32338800adc31439dbc79a
Roy Ivy III <rivy.dev@gmail.com>
2019-10-17 02:45:04 -0500

    change ~ lint ./eg

--- eg/show-parsed.js
+++ eg/show-parsed.js
@@ -1,21 +1,26 @@
 // spell-checker:ignore retVal
+/* eslint quotes: ["error", "single", { "avoidEscape": true }] */
 const parser = require('..');
 
 function stringify(s) {
-  var retval = JSON.stringify(s);
-  if (retval.search("'") >= 0) { return retval; }
+  let retval = JSON.stringify(s);
+
+  if (retval.search("'") >= 0) {
+    return retval;
+  }
+
   retval = retval.replace(/^"(.*)"$/, "'$1'").replace(/\\"/g, '"');
-  return retval
+  return retval;
 }
 
-function show_parsed(s) {
-  console.log(stringify(s), '=>', parser(s)); // eslint-disable-line unicorn/no-console-spaces
+function showParsed(s) {
+  console.log(stringify(s), '=>', parser(s));
 }
 
-show_parsed('{ }');
-show_parsed(' {key=value}');
-show_parsed('{.class}');
-show_parsed('{ ."class with  spaces +\\r\\n EOLs\\x03\\n" }');
-show_parsed('{ ##id }');
-show_parsed('{ key=value #"id with space" .class.x.y }');
-show_parsed("{ key=value #'id with space (single-quoted)' .class.x.y }");
+showParsed('{ }');
+showParsed(' {key=value}');
+showParsed('{.class}');
+showParsed('{ ."class with  spaces +\\r\\n EOLs\\x03\\n" }');
+showParsed('{ ##id }');
+showParsed('{ key=value #"id with space" .class.x.y }');
+showParsed("{ key=value #'id with space (single-quoted)' .class.x.y }");
