98663dd128ecf7561da8301180c5b6a2acd60358
Roy Ivy III <rivy.dev@gmail.com>
2019-10-17 02:33:26 -0500

    change ~ polish examples

--- eg/load.js
+++ eg/load.js
@@ -1,11 +1,21 @@
+// spell-checker:ignore retVal
 const parser = require('..');
 
-function show(s) {
-  console.log(s, ' => ', parser(s)); // eslint-disable-line unicorn/no-console-spaces
+function stringify(s) {
+  var retval = JSON.stringify(s);
+  if (retval.search("'") >= 0) { return retval; }
+  retval = retval.replace(/^"(.*)"$/, "'$1'").replace(/\\"/g, '"');
+  return retval
 }
 
-show('{ }');
-show('{ .class }');
-show('{ ."class with  spaces +\\r\\n EOLs\\x03\\n" }');
-show('{ #id }');
-show('{ key=value #id .class.x.y }');
+function log_parsed(s) {
+  console.log(stringify(s), ' => ', parser(s)); // eslint-disable-line unicorn/no-console-spaces
+}
+
+log_parsed('{ }');
+log_parsed(' {key=value}');
+log_parsed('{.class}');
+log_parsed('{ ."class with  spaces +\\r\\n EOLs\\x03\\n" }');
+log_parsed('{ ##id }');
+log_parsed('{ key=value #"id with space" .class.x.y }');
+log_parsed("{ key=value #'id with space (single-quoted)' .class.x.y }");
